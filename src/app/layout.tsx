0607aecc59fb69a62a610e39ca071a65d09ff6af
Tamara1430 <103314364+Tamara1430@users.noreply.github.com>
2025-07-30 20:38:36 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-aimport type { ReactNode } from 'react';
+import type { ReactNode } from 'react';
 import './globals.css';
 import { PaketKelasProvider } from '@/app/context/PaketKelasContext'; // ⬅️ Import Provider saja
 
